40c2406fd919c4a2af86cb3739188f65053e9b67
xieyue86 <xieyue86@163.com>
2019-04-14 17:18:21 +0800

    save

---
+++ mongodemo/src/main/java/com/destiny/mongodemo/entity/Elephant.java
@@ -0,0 +1,60 @@
+package com.destiny.mongodemo.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document
+public class Elephant implements Serializable {
+
+    private static final long serialVersionUID = -6391159311665543019L;
+
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private String address;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Elephant{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
