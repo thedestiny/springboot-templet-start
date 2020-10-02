635f2d6f2e4ad9c435cb6bd01c1ebb57e8bc4d68
xieyue86 <xieyue86@163.com>
2020-10-02 22:20:57 +0800

    mybatis-plus 添加乐观锁插件

--- springboot-freemarker-mybatisplus-hikari/src/main/java/com/destiny/rabbit/config/MybatisPlusConfig.java
+++ springboot-freemarker-mybatisplus-hikari/src/main/java/com/destiny/rabbit/config/MybatisPlusConfig.java
@@ -1,5 +1,6 @@
 package com.destiny.rabbit.config;
 
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,15 @@ public class MybatisPlusConfig {
 		PaginationInnerInterceptor paginationInterceptor = new PaginationInnerInterceptor();
 		return paginationInterceptor;
 	}
+	
+	
+	//注册乐观锁插件
+	@Bean
+	public OptimisticLockerInnerInterceptor optimisticLockerInterceptor() {
+		return new OptimisticLockerInnerInterceptor();
+	}
+	
+	
 	/*@Bean
 	public PaginationInterceptor paginationInterceptor() {
 		PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
