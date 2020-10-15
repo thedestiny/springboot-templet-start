cbdc4da09c5c39fcd81464471963c88b677b6335
xieyue86 <xieyue86@163.com>
2020-10-15 10:00:25 +0800

    更新 junit jar 包

--- springboot-thymeleaf/src/main/java/com/destiny/wolf/WolfApplication.java
+++ springboot-thymeleaf/src/main/java/com/destiny/wolf/WolfApplication.java
@@ -1,12 +1,12 @@
 package com.destiny.wolf;
 
-import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
+// import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
 import com.destiny.wolf.config.WolfAppEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+// import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
