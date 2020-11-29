06bc54deebdc5bb08512a592699a1c3937c79715
xieyue86 <xieyue86@163.com>
2020-11-29 19:54:49 +0800

    添加 shell 脚本

--- spring-jsp/src/main/java/com/destiny/dog/util/JavaReferenceTest.java
+++ spring-jsp/src/main/java/com/destiny/dog/util/JavaReferenceTest.java
@@ -7,6 +7,7 @@ public class JavaReferenceTest {
 	
 	public static void main(String[] args) {
 		
+		// -128 到 127 是在缓存中,因此 == 是相等的,超出范围则是不相等
 		Integer d1 = 3;
 		Integer d2 = 3;
 		Integer d3 = 300;
