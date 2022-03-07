51ea9160748447bc7ba4e845f3d7088243004625
xieyue86 <xieyue86@163.com>
2022-03-07 23:17:53 +0800

    代码修改

--- spring-jsp/src/main/java/com/destiny/dog/learn/io/bio/DogBioClient.java
+++ spring-jsp/src/main/java/com/destiny/dog/learn/io/bio/DogBioClient.java
@@ -5,12 +5,19 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 
 public class DogBioClient {
 	
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		int count = 100;
+
+		// 异步执行 CompletableFuture
+		CompletableFuture
+				.runAsync( () -> System.out.println("start app1"))
+				.thenRun( () -> System.out.println("start app2"))
+				.thenRun( () -> System.out.println("start app3"));
 		
 		final CountDownLatch latch = new CountDownLatch(count);
 		
