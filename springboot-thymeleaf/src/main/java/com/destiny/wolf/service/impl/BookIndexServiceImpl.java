faa8b32c99a330c76ae2f7d240628ddb1f8b3657
xieyue86 <xieyue86@163.com>
2023-04-19 15:53:08 +0800

    添加统计信息

--- springboot-thymeleaf/src/main/java/com/destiny/wolf/service/impl/BookIndexServiceImpl.java
+++ springboot-thymeleaf/src/main/java/com/destiny/wolf/service/impl/BookIndexServiceImpl.java
@@ -8,8 +8,10 @@ import com.destiny.wolf.service.BookIndexService;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.stereotype.Service;
 
@@ -23,12 +25,12 @@ import java.util.Map;
 @Slf4j
 @Service
 public class BookIndexServiceImpl extends IEsServiceImpl implements BookIndexService {
-	
+
 	/**
 	 * 索引名
 	 * */
 	private static final String index = "book_index";
-	
+
 	@Override
 	public void insertBach(List<BookIndex> list) {
 		if (list.isEmpty()) {
@@ -39,7 +41,7 @@ public class BookIndexServiceImpl extends IEsServiceImpl implements BookIndexSer
 			super.insertRequest(index, student.getId().toString(), student);
 		});
 	}
-	
+
 	@Override
 	public List<BookIndex> searchList() {
 		SearchResponse searchResponse = search(index);
@@ -51,31 +53,39 @@ public class BookIndexServiceImpl extends IEsServiceImpl implements BookIndexSer
 			// 转换查询对象
 			BookIndex index = JSONObject.parseObject(hit.getSourceAsString(), BookIndex.class);
 			Map<String,String> map = new HashMap<>();
-			
+
 			lolList.add(lol);
 		});
 		return lolList;
 	}
-	
+
 	protected SearchResponse search(String index) {
 
 		SearchRequest searchRequest = new SearchRequest(index);
 		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
 		searchSourceBuilder.trackTotalHits(true);
 		searchSourceBuilder.query(QueryBuilders.matchAllQuery());
-		searchSourceBuilder.query(QueryBuilders.rangeQuery("level").gte("34"));
-		
+		searchSourceBuilder
+				.query(QueryBuilders.rangeQuery("level").gte("34"))
+				.query(QueryBuilders.termQuery("", ""));
+
 		//bool符合查询
 		//BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder()
 		//        .filter(QueryBuilders.matchQuery("name", "盖伦"))
 		//        .must(QueryBuilders.matchQuery("desc", "部落"))
 		//        .should(QueryBuilders.matchQuery("realName", "光辉"));
-		
+
+		BoolQueryBuilder level = QueryBuilders.boolQuery()
+				.must(QueryBuilders.rangeQuery("level").gte("34").lte("45"))
+				.must(QueryBuilders.termQuery("mi", "val"));
+		searchSourceBuilder.query(level);
+
+
 		//分页
 		//searchSourceBuilder.from(1).size(2);
 		// 排序
 		//searchSourceBuilder.sort("", SortOrder.DESC);
-		
+
 		////误拼写时的fuzzy模糊搜索方法 2表示允许的误差字符数
 		//QueryBuilders.fuzzyQuery("title", "ceshi").fuzziness(Fuzziness.build("2"));
 		searchRequest.source(searchSourceBuilder);
@@ -89,7 +99,32 @@ public class BookIndexServiceImpl extends IEsServiceImpl implements BookIndexSer
 		}
 		return searchResponse;
 	}
-	
-	
-	
+
+
+	public static void main(String[] args) {
+
+		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+		searchSourceBuilder.trackTotalHits(true);
+//		searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+//		searchSourceBuilder
+//				.query(QueryBuilders.rangeQuery("level").gte("34"))
+//				.query(QueryBuilders.termQuery("34", "44"));
+
+		//bool符合查询
+		//BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder()
+		//        .filter(QueryBuilders.matchQuery("name", "盖伦"))
+		//        .must(QueryBuilders.matchQuery("desc", "部落"))
+		//        .should(QueryBuilders.matchQuery("realName", "光辉"));
+
+		BoolQueryBuilder level = QueryBuilders.boolQuery()
+				.must(QueryBuilders.rangeQuery("level").gte("34").lte("45"))
+				.must(QueryBuilders.termQuery("mi", "val"));
+		searchSourceBuilder.query(level);
+
+
+
+		System.out.println(searchSourceBuilder.toString());
+
+	}
+
 }
